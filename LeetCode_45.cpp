16d0fead02f9586e84dc4b6c4e9e35ad104e53cd
zhangze <zhangze_8_8@sina.com>
2020-05-04 23:36:58 +0800

    0504

---
+++ LeetCode_45.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        int position = nums.length - 1;
+        int steps = 0;
+        while (position > 0) {
+            for (int i = 0; i < position; i++) {
+                if (i + nums[i] >= position) {
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/jump-game-ii/solution/tiao-yue-you-xi-ii-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
